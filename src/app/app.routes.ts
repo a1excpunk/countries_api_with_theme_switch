d4af7fbbe87c3e08d963aca6e6bbba971c8a3da9
Alexandra <a1excpunk@users.noreply.github.com>
2024-02-13 20:32:52 +0400

    ADD: routs to main, details and 404 pages; also redirection to main page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { DetailsPageComponent } from './details-page/details-page.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MainPageComponent } from './main-page/main-page.component';
+
+export const routes: Routes = [
+    { path: 'main', component: MainPageComponent },
+    { path: 'details-page', component: DetailsPageComponent },
+    { path: '', redirectTo: '/main', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+];
