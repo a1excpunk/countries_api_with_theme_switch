cf2f161a69d6f6af63029af7bec394021f395176
Alexandra <a1excpunk@users.noreply.github.com>
2024-02-13 20:40:34 +0400

    ADD: router modules in ts and routing to details page

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { CountryCardComponent } from '../country-card/country-card.component';
 import { SearchInputsComponent } from '../search-inputs/search-inputs.component';
-import { NgFor } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [CountryCardComponent, SearchInputsComponent, NgFor],
+  imports: [CountryCardComponent, SearchInputsComponent, NgFor, CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
 export class MainPageComponent {
-  items = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+  items = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  detailsPage() {
+    console.log('asdrgadsr');
+
+  }
 }
