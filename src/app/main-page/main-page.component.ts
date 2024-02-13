6b7659d6352dd70fc581c68a044562f3a648b589
Alexandra <a1excpunk@users.noreply.github.com>
2024-02-13 14:31:51 +0400

    ADD: ngFor loop on cards; with mockup data to test

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CountryCardComponent } from '../country-card/country-card.component';
 import { SearchInputsComponent } from '../search-inputs/search-inputs.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [CountryCardComponent, SearchInputsComponent],
+  imports: [CountryCardComponent, SearchInputsComponent, NgFor],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
 export class MainPageComponent {
-
+  items = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 }
