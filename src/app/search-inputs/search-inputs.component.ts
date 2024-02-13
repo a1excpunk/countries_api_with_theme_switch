886fa7a7970df665b03c058bba3729c33d05f020
Alexandra <a1excpunk@users.noreply.github.com>
2024-02-13 20:42:47 +0400

    ADD: region selector functionality mockup and text interpolation for dynamic data display

--- src/app/search-inputs/search-inputs.component.ts
+++ src/app/search-inputs/search-inputs.component.ts
@@ -1,12 +1,21 @@
+import { NgFor, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-search-inputs',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgStyle],
   templateUrl: './search-inputs.component.html',
   styleUrl: './search-inputs.component.scss'
 })
 export class SearchInputsComponent {
-
+  regionPlaceholder: string = "Filter by Region"
+  regions = ["test 1", "test 2", "test 3", "test 4", "test 5"];
+  regionSelectorOpen: boolean = false;
+  selectRegion(value: string) {
+    this.regionPlaceholder = value;
+  }
+  toggleRegionSelector() {
+    this.regionSelectorOpen = !this.regionSelectorOpen;
+  }
 }
