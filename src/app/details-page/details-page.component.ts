d2c49ed1d64673ff4ebd4d11596a74ec3bca3ee6
Alexandra <a1excpunk@users.noreply.github.com>
2024-02-13 20:38:20 +0400

    ADD: router modules in ts and testing text and button to navigate back to main page

--- src/app/details-page/details-page.component.ts
+++ src/app/details-page/details-page.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-details-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './details-page.component.html',
   styleUrl: './details-page.component.scss'
 })
