8a758f0db25a3a855d64094d77e9e671c3742d56
Alexandra <a1excpunk@users.noreply.github.com>
2024-02-17 22:18:50 +0400

    ADD: custom pipe for separating list items with comma

---
+++ src/app/comma.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'comma',
+  standalone: true
+})
+export class CommaPipe implements PipeTransform {
+
+  transform(element: string, arrayLength: number, elementIndex: number): any {
+    if (arrayLength > 1 && elementIndex !== arrayLength - 1) {
+      return element + ", "
+    } else {
+      return element;
+    }
+  }
+}
